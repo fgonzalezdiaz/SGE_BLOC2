2f83d5238cef197f876a2e86199a0e86fe604377
fran <fgonzalezd24042005@gmail.com>
2025-03-11 10:20:30 +0100

    Sense acabar

--- postgresql_python/main.py
+++ postgresql_python/main.py
@@ -1,9 +1,5 @@
-import read_registre as rr
-results = rr.read_reg()
-for i in results:
-       print('\n')
-       print('Nom: ' + i[1])
-       print('Adreça: ' + i[2])
-       print('telèfon: ' + i[3])
-       print('email: ' + i[4])
-       print('naixement: ' + i[5])
+import create_registre as cr
+
+
+#Trucada per executar la funció a l'arxiu create_registre.py
+cr.create_reg()
--- postgresql_python/read_registre.py
+++ postgresql_python/read_registre.py
@@ -3,7 +3,7 @@ def read_reg():
     conn = connect.connection_db()
     cursor = conn.cursor()
 
-    sql_read = "SELECT * FROM clientes2"
+    sql_read = "SELECT * FROM Clientes"
 
     cursor.execute(sql_read)
     conn.commit()
